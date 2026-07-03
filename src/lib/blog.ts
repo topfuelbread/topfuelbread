86077281279110a3ba8f20a98f7fe65f55291803
June <40782646+topfuelbread@users.noreply.github.com>
2026-07-03 18:35:50 -0400

    feat: add functions to retrieve specific blog post and latest blog post ID

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,10 +1,20 @@
-import { getCollection, type CollectionEntry } from "astro:content";
+import { getCollection, getEntry, type CollectionEntry } from "astro:content";
 
 type BlogPost = CollectionEntry<"blog">;
+
+export async function getBlogPost(id: string): Promise<BlogPost | undefined> {
+  const post = await getEntry("blog", id);
+  return post ?? undefined;
+}
 
 export async function getLatestBlogPost(): Promise<BlogPost | undefined> {
   const posts = await getCollection("blog");
   return posts.sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
   )[0];
+}
+
+export async function getLatestBlogPostId(): Promise<string | undefined> {
+  const post = await getLatestBlogPost();
+  return post?.id;
 }
